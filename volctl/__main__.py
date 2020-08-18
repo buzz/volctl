67a1ec5ecc3ccd50c9630ce2d058aa65ea91f774
buzz <buzz@users.noreply.github.com>
2020-08-18 11:26:56 +0200

    Add Gtk.init() call, refactor __main__.py

--- volctl/__main__.py
+++ volctl/__main__.py
@@ -3,17 +3,20 @@
 """volctl main entry point."""
 
 
+"""Start volctl."""
+import gi
+
+gi.require_version("Gdk", "3.0")
+gi.require_version("GdkX11", "3.0")
+gi.require_version("Gio", "2.0")
+gi.require_version("GLib", "2.0")
+gi.require_version("GObject", "2.0")
+gi.require_version("Gtk", "3.0")
+from gi.repository import Gtk
+from volctl.app import VolctlApp
+
+
 def main():
-    """Start volctl."""
-    import gi
-
-    gi.require_version("Gtk", "3.0")
-    from gi.repository import Gtk
-    from volctl.app import VolctlApp
-
+    Gtk.init()
     VolctlApp()
     Gtk.main()
-
-
-if __name__ == "__main__":
-    main()
