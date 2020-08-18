343943f74d1df33d28f2d7c8c042d8cd2847d9d2
buzz <buzz@users.noreply.github.com>
2020-08-18 12:29:38 +0200

    v0.7.8

--- volctl/meta.py
+++ volctl/meta.py
@@ -1,7 +1,7 @@
 """Version string used by program and setup.py (using regex parsing)."""
 from gi.repository import Gtk
 
-VERSION = "0.7.7"
+VERSION = "0.7.8"
 PROGRAM_NAME = "Volume Control"
 COPYRIGHT = "(c) buzz"
 LICENSE = Gtk.License.GPL_2_0
