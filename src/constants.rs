93a1e1a04e36ef565502f207c4311ae85fe26928
buzz <buzz@users.noreply.github.com>
2024-11-13 23:07:53 +0100

    chore: define settings key constants

--- src/constants.rs
+++ src/constants.rs
@@ -8,4 +8,13 @@ pub const MAX_SCALE_VOL: u32 = (MAX_NATURAL_VOL as f64 * 1.5) as u32;
 
 // Setting keys
 pub const SETTINGS_MOUSE_WHEEL_STEP: &str = "mouse-wheel-step";
+pub const SETTINGS_MIXER_COMMAND: &str = "mixer-command";
+pub const SETTINGS_SHOW_PERCENTAGE: &str = "show-percentage";
+pub const SETTINGS_VU_ENABLED: &str = "vu-enabled";
+pub const SETTINGS_AUTO_CLOSE: &str = "auto-close";
+pub const SETTINGS_TIMEOUT: &str = "timeout";
 pub const SETTINGS_ALLOW_EXTRA_VOLUME: &str = "allow-extra-volume";
+pub const SETTINGS_OSD_ENABLED: &str = "osd-enabled";
+pub const SETTINGS_OSD_TIMEOUT: &str = "osd-timeout";
+pub const SETTINGS_OSD_SCALE: &str = "osd-scale";
+pub const SETTINGS_OSD_POSITION: &str = "osd-position";
